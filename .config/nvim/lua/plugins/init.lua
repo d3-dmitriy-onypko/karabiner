49683c88aa0fcc182786d428c0980ea012de4413
Dmitriy Onypko <vaporif@gmail.com>
2024-08-19 15:12:37 +0100

    remove lvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -122,5 +122,13 @@ return {
         highlight = { enable = true },
         indent = { enable = true }
       }
+    },
+    {
+       "jiaoshijie/undotree",
+      dependencies = "nvim-lua/plenary.nvim",
+      config = true,
+      keys = { -- load the plugin only when using it's keybinding:
+        { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+      }, 
     }
   }
