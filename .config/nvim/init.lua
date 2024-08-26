e6f4f611fca258599035e2a1c489285401a65af9
Dmitriy Onypko <vaporif@gmail.com>
2024-08-26 09:25:59 +0100

    refactor

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -84,14 +84,7 @@ require('lazy').setup({
   'tpope/vim-sleuth',
   require 'plugins.gitsigns',
   require 'plugins.hop',
-  {
-    'jiaoshijie/undotree',
-    dependencies = 'nvim-lua/plenary.nvim',
-    config = true,
-    keys = { -- load the plugin only when using it's keybinding:
-      { '<leader>u', "<cmd>lua require('undotree').toggle()<cr>", desc = '[U]ndotree' },
-    },
-  },
+  require 'plugins.undotree',
   {
     'mrcjkb/rustaceanvim',
     version = '^5',
